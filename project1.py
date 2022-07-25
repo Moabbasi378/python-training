502e5a7dbe8dd77c1d56493f467e3bf57c89df71
mohammad abbasi <mohammadabbasi378@gamil.com>
2022-07-25 12:48:16 +0430

    traing for while and input

--- project1.py
+++ project1.py
@@ -3,17 +3,18 @@ print(" /|_||_\`.__")
 print("(   _    _ _\ ")
 print("=`-(_)--(_)-' ")
 print("welcome to the game!")
+
 while 0 < 1 :
-    a = input("enter your order(start,stop,help,quit): ").lower()
-    if a == "start":
+    order = input("enter your order(start,stop,help,quit): ").lower()
+    if order == "start":
         print("\t\t       ______")
         print("\t\t      /|_||_\`.__")
         print("\t\t     (   _    _ _\ ")
         print("\t\t     =`-(_)--(_)-' ")
         print("car moved")
-    elif a == "stop": print("car stopped")
-    elif a == "help":
+    elif order == "stop": print("car stopped")
+    elif order == "help":
         print("start = moving car \n stop = stopping car\n quit = exit the game ")
-    elif a == "quit":
+    elif order == "quit":
         print("goodbye")
         break
